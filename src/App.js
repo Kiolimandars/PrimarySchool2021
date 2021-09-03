a7875b50719e892759a710082303701443a512bf
Your NameAziz-Dhiab <aziz_dhiab@hotmail.com>
2021-09-03 11:11:05 +0100

    aziz commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ButtonAppBar from "./Components/ButtonAppBar";
+import { Container } from './Components/Container';
 
-function App() {
+
+const App = () => {
+  const triggerText = 'Open form';
+  const onSubmit = (event) => {
+    event.preventDefault(event);
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+  };
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <ButtonAppBar/>
+      <Container triggerText={triggerText} onSubmit={onSubmit} />
     </div>
+    
   );
+  
 }
 
-export default App;
+export default App
